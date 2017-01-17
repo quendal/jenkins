d5c46314affccdc972a0737a6b4bc18d6ff1266a
Quendal <quendal@gmail.com>
2017-01-17 10:48:28 -0600

    Primer comentario

---
+++ my-app/src/main/java/com/microservicios/app/App.java
@@ -0,0 +1,13 @@
+package com.microservicios.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
